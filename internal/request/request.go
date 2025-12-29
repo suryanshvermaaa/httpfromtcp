2a524c630b2e65f034b0a13fd190482fb5a30f9f
Suryansh Verma <suryanshverma.dev.cloud@gmail.com>
2025-12-29 16:43:16 +0530

    feat: implement request parsing and validation logic with unit tests

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,67 @@
 package request
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+var ERROR_MALFORMED_REQUEST_LINE = fmt.Errorf("malformed request-line")
+var ERROR_UNSUPPORTED_HTTP_VERSION = fmt.Errorf("unsupported http version")
+var SAPARATOR = "\r\n"
+
+func (r *RequestLine) ValidHTTP() bool {
+	return r.HttpVersion == "HTTP/1.1"
+}
+
+func parseRequestLine(b string) (*RequestLine, string, error) {
+	idx := strings.Index(b, SAPARATOR)
+	if idx == -1 {
+		return nil, b, nil
+	}
+
+	startOfLine := b[:idx]
+	restOfLine := b[idx+len(SAPARATOR):]
+
+	parts := strings.Split(startOfLine, " ")
+	if len(parts) != 3 {
+		return nil, b, ERROR_MALFORMED_REQUEST_LINE
+	}
+	httpParts := strings.Split(parts[2], "/")
+	if len(httpParts) != 2 || httpParts[0] != "HTTP" || httpParts[1] != "1.1" {
+		return nil, restOfLine, ERROR_MALFORMED_REQUEST_LINE
+	}
+	rl := &RequestLine{
+		Method:        parts[0],
+		RequestTarget: parts[1],
+		HttpVersion:   httpParts[1],
+	}
+	return rl, restOfLine, nil
+
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, errors.Join(fmt.Errorf("unable to io.RealAll"), err)
+	}
+	str := string(data)
+	rl, str, err := parseRequestLine(str)
+	if err != nil {
+		return nil, err
+	}
+	return &Request{
+		RequestLine: *rl,
+	}, err
+}
