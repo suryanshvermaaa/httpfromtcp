abe3755ae3bf2e1ec16d5fa33be80a541dc42346
Suryansh Verma <suryanshverma.dev.cloud@gmail.com>
2025-12-29 14:49:23 +0530

    refactor: extract line reading logic into a separate function

--- main.go
+++ main.go
@@ -3,32 +3,48 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+			str += string(data)
+		}
+		if len(str) != 0 {
+			out <- str
+		}
+	}()
+	return out
+}
 
 func main() {
 	f, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-		str += string(data)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", string(line))
 	}
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
-	}
+
 }
